862bd2682b818a3bde23c8ec8bc0111223030918
Juan Felipe <juanfecazuluaga@gmail.com>
2024-04-12 07:55:51 -0500

    basemodel pydantic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
+from pydantic import BaseModel as BaseModelPydantic
 
 import os
 from typing import List
@@ -19,8 +19,8 @@ class Resume(BaseModel):
     experience: list = Field(description="experiencia laboral del candidato. Cada experiencia es un json con keys company (compañia), dates (fecha) y role (cargo). Si no encuentras uno de los valores pon un string vacio. Si no encuentras fecha para esa experiencia pon '-' en la fecha")
     education: list = Field(description="educación del candidato. De cada uno extraer: Universidad, estudio y fecha. Cada educacion es un json con keys university (universidad), dates (fecha) y program (estudio realizado). Si no encuentras fecha para ese estudio pon '-' en la fecha")
 
-class Request(BaseModel):
-    resume: str = Field(description="CV de la persona en un string")
+class Request(BaseModelPydantic):
+    resume: str
 
 app = FastAPI()
 app.add_middleware(
